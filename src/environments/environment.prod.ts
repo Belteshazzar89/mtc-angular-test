ff0d0006be0898e81f350e0affc285e0192c616e
Sterling Coleman <sterling.coleman@algoan.com>
2020-04-23 21:34:13 +0200

    firebase for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC9f8AxpCd76752526ulJm4DGKxi6Y08Hg',
+    authDomain: 'users-aa6ac.firebaseapp.com',
+    databaseURL: 'https://users-aa6ac.firebaseio.com',
+    projectId: 'users-aa6ac',
+    storageBucket: 'users-aa6ac.appspot.com',
+    messagingSenderId: '109043323178',
+  },
 };
